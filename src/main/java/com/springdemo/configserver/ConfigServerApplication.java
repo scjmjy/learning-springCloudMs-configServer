913cadbb999e7369819a5efda77f2c23acf16710
jinyuan ming <mjyok12345@gmail.com>
2021-11-29 14:50:54 +0800

    initial commit

--- src/main/java/com/springdemo/configserver/ConfigServerApplication.java
+++ src/main/java/com/springdemo/configserver/ConfigServerApplication.java
@@ -1,9 +1,11 @@
-package scj.mjy.java.configserver;
+package com.springdemo.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
